97f050e772a82c1a676ee606484b576eb68caa93
Vishnu Vardhan Singh <sugam.singh3@gmail.com>
2025-12-11 22:41:54 +0530

    initial state

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,6 +1,5 @@
 export default function SearchBox(){
     return <>
-    div 
-    <input type="text" className="border-2"/>
+    <div className='flex'><img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/fkheaderlogo_exploreplus-44005d.svg" alt="flipkart logo" /><input type="text" className="border-2"/></div>
     </>
 }
