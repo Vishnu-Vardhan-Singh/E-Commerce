c50f8f9707e7d5763a29d9e6a601d4c0d8224acf
Vishnu Vardhan Singh <sugam.singh3@gmail.com>
2025-12-13 17:01:07 +0530

    custom hook created for theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Home from './components/Home'
 import SearchBox from './components/SearchBox'
 import Counter from './Custom Hook/counter'
+import Demo from './Custom Hook/LocalStorage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
       <SearchBox/>
       <Home/>
       <Counter/>
+      <Demo/>
     </>
   )
 }
--- src/Custom Hook/counter.jsx
+++ src/Custom Hook/counter.jsx
@@ -1,15 +1,44 @@
 import { useState } from "react";
 
 export default function Counter() {
-  const [count, setCount] = useState('Ram Ram');
+  const {value,input,add,subtract,reset} = useCount();
   return (
-        <div className="flex [&>*]:border-2 [&>*]:mx-1 [&>*]:p-1 [&>*]:rounded-2xl" >
+        <div className="flex [&>*]:border-2 [&>*]:mx-1 [&>*]:p-1 [&>*]:rounded-2xl [&>*]:hover:cursor-pointer" >
         
-        <input type="text"  />
-        <button>add</button>
-        <button>Remove</button>
-        <button>Reset</button>
-        <div>{count}</div>
+        <input className='!rounded-[0px] !cursor-default'  type="text" placeholder='Enter value' onChange={input} />
+        <button onClick={add}>Addition</button>
+        <button onClick={subtract}>Subtraction</button>
+        <button onClick={reset}>Reset</button>
+        <div className='!rounded-[0px] !cursor-default'>{value}</div>
         </div>
   );
+}
+
+
+function useCount(){
+        const [value,setValue]=useState(0)
+        const [value1,setValue1] = useState(1)
+        function input(e){
+            setValue1(()=> Number(e.target.value))
+        }
+        function add(arg){
+              return function(){
+                    setValue((val)=>val+arg)
+              }
+        }
+        function subtract(arg){
+              return function(){
+                    setValue((val)=>val-arg)
+              }
+        }
+        function reset(){
+              setValue(()=>0)
+        }
+        return {
+            value,
+            input,
+            add:add(value1),
+            subtract:subtract(value1),
+            reset:reset
+        }
 }
